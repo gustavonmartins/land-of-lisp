5cbf167c6771897f2c6d14033824d2fd29acf673
Gustavo Nunes Martins <martins.yo@gmail.com>
2017-01-02 19:02:47 +0100

    dice of doom v2 included

--- dice-of-doom/dice-of-doom-v1.lisp
+++ dice-of-doom/dice-of-doom-v1.lisp
@@ -1,6 +1,8 @@
+;chapter 15
+
 (defparameter *num-players* 2)
 (defparameter *max-dice* 3)
-(defparameter *board-size* 3)
+(defparameter *board-size* 2)
 (defparameter *board-hexnum* (* *board-size* *board-size*))
 
 (defun board-array (lst)
